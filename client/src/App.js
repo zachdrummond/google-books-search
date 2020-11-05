834a9bc04c3726d7c96fa3f34449b580233c22a7
Zach Drummond <zachdrummond3@gmail.com>
2020-11-05 18:54:54 -0500

    Added router and switch with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./components/Saved/Saved";
+import Search from "./components/Search/Search";
+
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Switch>
+          <Route exact path="/saved" component={Saved} />
+          <Route exact path="/search" component={Search} />
+          <Route path="/" component={Saved} />
+        </Switch>
+    </Router>
   );
 }
 
