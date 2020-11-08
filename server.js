29a0e0e088a38ace633ba070fa4fc5f0f2a62cad
Zach Drummond <zachdrummond3@gmail.com>
2020-11-08 14:37:05 -0500

    Update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/hogwarts-library",
+  process.env.MONGODB_URI || "mongodb://localhost/google-books-search",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
